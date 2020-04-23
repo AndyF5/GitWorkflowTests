341f916022a928f89efc8311d723fe5118b54881
Andrew Faulkner <a.g.flknr@gmail.com>
2020-04-23 14:02:09 -0400

    Say hello world.

---
+++ GitWorkflowTests/GitWorkflowTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWorkflowTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to close...");
+
+            Console.ReadKey();
+        }
+    }
+}
